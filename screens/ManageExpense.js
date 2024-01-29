7ce0a54acd4a3956f78d1cd7258cf274f4698b53
Wasim Hyder <wasim@creativefaze.com>
2024-01-29 21:58:29 +0500

    Added reuseable button component

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -2,6 +2,7 @@ import { useLayoutEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
+import Button from '../components/UI/Button';
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -17,7 +18,19 @@ function ManageExpense({ route, navigation }) {
 
   }
 
+  function cancelHandler() {
+
+  }
+
+  function confirmHandler() {
+
+  }
+
   return (<View style={styles.container}>
+    <View style={styles.buttons}>
+      <Button style={styles.button} mode='flat' onPress={cancelHandler}>Cancel</Button>
+      <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+    </View>
     {isEditing && (
       <View style={styles.deleteContainer}>
         <IconButton icon='trash' color={GlobalStyles.colors.error500} size={36} onPress={deleteExpenseHandler} />
@@ -33,6 +46,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8
   },
   deleteContainer: {
     marginTop: 16,
