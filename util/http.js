08df418bfd434912584f7e6b6e78f285fb2d0265
Wasim Hyder <wasim@creativefaze.com>
2024-02-03 20:21:40 +0500

    Fetched expenses from backend

--- util/http.js
+++ util/http.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
+const BACKEND_URL = 'https://react-expense-manager-3970d-default-rtdb.firebaseio.com/';
 export function storeExpense(expenseData) {
-  axios.post('https://react-expense-manager-3970d-default-rtdb.firebaseio.com/expenses.json', expenseData)
+  axios.post(BACKEND_URL + 'expenses.json', expenseData)
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + 'expenses.json');
+  const expenses = [];
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    }
+    expenses.push(expenseObj);
+  }
+  return expenses;
 }
